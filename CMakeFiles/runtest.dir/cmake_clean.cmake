8bf0882b06d7ef26647815b85ef190362644c748
Luciano Mantovani <lucianomantovani@Lucianos-MacBook-Air.local>
2018-12-20 12:48:25 -0300

    an initial makefile using cmake. compiles tests and runs.

---
+++ CMakeFiles/runtest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runtest.dir/test/010-TestCase.cpp.o"
+  "runtest.pdb"
+  "runtest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runtest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "sum.hpp"
+
+using namespace std;
+
+int main ()
+{
+	cout << "hello, world!" << endl;
+
+	cout << "this is a sum:" << endl;
+
+	cout << sum(4,5) << endl;
+
+	return 0;
+}
