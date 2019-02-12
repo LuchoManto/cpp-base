d5725472c63704c7f27c3ca50a53d8db5a288f35
Luciano Mantovani <lucianomantovani@Lucianos-MacBook-Air-438.local>
2019-02-12 12:55:47 -0300

    manejo de login separado en Controller y MyApp reducido

--- perl-practice/mojolicious_practice/myapp/lib/MyApp.pm
+++ perl-practice/mojolicious_practice/myapp/lib/MyApp.pm
@@ -10,32 +10,12 @@ sub startup {
   $self->helper(users => sub { state $users = MyApp::Model::Users->new });
 
   my $r = $self->routes;
+  $r->any('/')->to('login#index')->name('index');
 
-  $r->any('/' => sub {
-    my $c = shift;
+  my $logged_in = $r->under('/')->to('login#logged_in');
+  $logged_in->get('/protected')->to('login#protected');
 
-    my $user = $c->param('user') || '';
-    my $pass = $c->param('pass') || '';
-    return $c->render unless $c->users->check($user, $pass);
-
-    $c->session(user => $user);
-    $c->flash(message => 'Thanks for logging in.');
-    $c->redirect_to('protected');
-  } => 'index');
-
-  my $logged_in = $r->under(sub {
-    my $c = shift;
-    return 1 if $c->session('user');
-    $c->redirect_to('index');
-    return undef;
-  });
-  $logged_in->get('/protected');
-
-  $r->get('/logout' => sub {
-    my $c = shift;
-    $c->session(expires => 1);
-    $c->redirect_to('index');
-  });
+  $r->get('/logout')->to('login#logout');
 }
 
 1;
